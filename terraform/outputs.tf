ec193be19e49e62ac87c4d26e71549795f7250b2
Caleb Kalu <kalebiotezz@yahoo.com>
2026-08-04 00:42:08 +0100

    output to remove sensitive output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,7 +9,3 @@ output "acr_login_server" {
 output "aks_cluster_name" {
   value = azurerm_kubernetes_cluster.aks.name
 }
-
-output "aks_kube_config" {
-  value = azurerm_kubernetes_cluster.aks.kube_config_raw
-}
