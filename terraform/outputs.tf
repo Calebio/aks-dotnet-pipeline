8a5acc7c03b9299a37a769be63ffe321ea09fc5a
Caleb Kalu <kalebiotezz@yahoo.com>
2026-08-04 00:31:34 +0100

    updated output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,3 +9,7 @@ output "acr_login_server" {
 output "aks_cluster_name" {
   value = azurerm_kubernetes_cluster.aks.name
 }
+
+output "aks_kube_config" {
+  value = azurerm_kubernetes_cluster.aks.kube_config_raw
+}
